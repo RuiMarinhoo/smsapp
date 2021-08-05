0fea3ab52371d745642e333db340a286d5eed1c9
Rui Marinho <ruijorgemm@hotmail.com>
2021-08-05 14:01:48 +0100

    custom-pag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
-import { CustomCallComponent } from './custom-call/custom-call.component';
+import { CustomCallComponent } from './home/custom-call/custom-call.component';
+import { HomeComponent } from './home/home.component';
 
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomCallComponent
+    CustomCallComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
